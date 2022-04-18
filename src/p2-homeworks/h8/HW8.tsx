e21bde246d51512610a1c07342f876e17626784d
Anton Galich <inthesky133@gmail.com>
2022-04-18 09:21:40 +0300

    add hw8

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,48 +1,64 @@
-import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
-import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import React, { useState } from "react";
+import { homeWorkReducer } from "./bll/homeWorkReducer";
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
-// export type UserType =
+export type UserType = {
+  _id: number;
+  name: string;
+  age: number;
+};
 
-const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
-]
+const initialPeople: UserType[] = [
+  { _id: 0, name: "Кот", age: 3 },
+  { _id: 1, name: "Александр", age: 66 },
+  { _id: 2, name: "Коля", age: 16 },
+  { _id: 3, name: "Виктор", age: 44 },
+  { _id: 4, name: "Дмитрий", age: 40 },
+  { _id: 5, name: "Ирина", age: 55 },
+];
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+  const [people, setPeople] = useState<UserType[]>(initialPeople); // need to fix any
 
-    // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
-        </div>
-    ))
+  // need to fix any
+  const finalPeople = people.map((p: UserType) => (
+    <div key={p._id}>
+      {p.name}, {p.age}
+    </div>
+  ));
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
-
-    return (
+  const sortUp = () =>
+    setPeople(homeWorkReducer(initialPeople, { type: "sort", payload: "up" }));
+  const sortDown = () =>
+    setPeople(
+      homeWorkReducer(initialPeople, { type: "sort", payload: "down" })
+    );
+  const isEighteen = () => {
+    setPeople(homeWorkReducer(initialPeople, { type: "check", payload: 18 }));
+  };
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        homeworks 8{/* should work (должно работать)*/}
+        {finalPeople}
         <div>
-            <hr/>
-            homeworks 8
-
-            {/*should work (должно работать)*/}
-            {finalPeople}
-
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativePeople/>*/}
-            <hr/>
+          <SuperButton onClick={sortUp}>sort up</SuperButton>
+          <SuperButton onClick={sortDown}>sort down</SuperButton>
+          <SuperButton onClick={isEighteen}>check 18</SuperButton>
         </div>
-    )
+        {/* для личного творчества, могу проверить*/}
+        {/* <AlternativePeople/>*/}
+      </div>
+      <hr />
+      <hr />
+    </>
+  );
 }
 
-export default HW8
+export default HW8;
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,28 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
-    }
+import { UserType } from "../HW8";
+interface IAction {
+  type: "sort" | "check";
+  payload: "up" | "down" | number;
 }
+export const homeWorkReducer = (
+  state: UserType[],
+  action: IAction
+): UserType[] => {
+  switch (action.type) {
+    case "sort": {
+      if (action.payload === "up") {
+        const newState = [...state];
+        return newState.sort((a, b) => (a.name > b.name ? 1 : -1));
+      }
+      if (action.payload === "down") {
+        const newState = [...state];
+        return newState.sort((a, b) => (a.name < b.name ? 1 : -1));
+      }
+      return state;
+    }
+    case "check": {
+      return state.filter((user) => user.age > action.payload);
+    }
+    default:
+      return state;
+  }
+};
