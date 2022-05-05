c48511793dcb4237cc80d63510db387788d263a9
Anton Galich <inthesky133@gmail.com>
2022-05-05 11:12:49 +0300

    add hw11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,39 +1,56 @@
-import React, {useState} from 'react'
-import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import React, { useState } from "react";
+import SuperRange from "./common/c7-SuperRange/SuperRange";
+import SuperDoubleRange from "./common/c8-SuperDoubleRange/SuperDoubleRange";
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(1000);
 
-    return (
+  const onChangeRange1 = (num: number) => {
+    setValue1(num);
+  };
+  const onChangeRange2 = (num: number) => {
+    setValue2(num);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <hr />
+        homeworks 11
+        {/* should work (должно работать)*/}
         <div>
-            <hr/>
-            homeworks 11
-
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
-
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
+          <span>{value1}</span>
+          <SuperRange
+            min={value1}
+            onChangeRange={onChangeRange1}
+            // сделать так чтоб value1 изменялось
+          />
         </div>
-    )
+        <div style={{ display: "flex" }}>
+          <span>{value1}</span>
+          <SuperDoubleRange
+            min={value1}
+            max={value2}
+            onChangeRangeMin={onChangeRange1}
+            onChangeRangeMax={onChangeRange2}
+          />
+          <span>{value2}</span>
+        </div>
+      </div>
+      <hr />
+      {/* для личного творчества, могу проверить*/}
+      {/* <AlternativeSuperRange/>*/}
+      {/* <AlternativeSuperDoubleRange/>*/}
+      <hr />
+    </>
+  );
 }
 
-export default HW11
+export default HW11;
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,52 @@
-import React from 'react'
-
+import React, { useRef, useState } from "react";
+import "./SuperDoubleRange.css";
+import classnames from "classnames";
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
-}
+  onChangeRangeMin: (value: number) => void;
+  onChangeRangeMax: (value: number) => void;
+  min: number;
+  max: number;
+};
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
-    }
-) => {
-    // сделать самому, можно подключать библиотеки
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = ({
+  onChangeRangeMin,
+  onChangeRangeMax,
+  min,
+  max,
+}) => {
+  return (
+    <>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={min}
+        onChange={(event) => {
+          const value = Math.min(+event.target.value, max - 1);
+          onChangeRangeMin(value);
+          event.target.value = value.toString();
+        }}
+        className={classnames("thumb thumb--zindex-3", {
+          "thumb--zindex-5": min > max - 100,
+        })}
+      />
+      <input
+        type="range"
+        min="0"
+        max="100"
+        onChange={(event) => {
+          const value = Math.max(+event.target.value, min + 1);
+          onChangeRangeMax(value);
+          event.target.value = value.toString();
+        }}
+        className="thumb thumb--zindex-4"
+      />
+      <div className="slider">
+        <div className="slider__track" />
+        <div className="slider__range" />
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            DoubleRange
-        </>
-    )
-}
-
-export default SuperDoubleRange
+export default SuperDoubleRange;
